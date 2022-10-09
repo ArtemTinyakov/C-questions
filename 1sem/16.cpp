dca4c1652f01559d733115dcc038b791c4a54537
ArtemTinyakov <artemtin@mail.ru>
2022-10-09 23:45:11 +0300

    :)

--- 1sem/16.cpp
+++ 1sem/16.cpp
@@ -3,5 +3,6 @@ int main()
     int x;
     (2 < 3 ? ++x : x++) = 5;             //What will happen here?
     (2 < 3 ? ++x : x) = 5;               //What will happen here?
+    (2 < 3 ? ++x : x++);
     return 0;
 }
